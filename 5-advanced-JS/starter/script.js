26ed4e503d5e4d145379c64eea924cf34815906b
Doug Allen <producerdoug@users.noreply.github.com>
2020-04-24 12:46:00 -0700

    Finish Section 5

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -227,7 +227,7 @@ function interviewQuestion(job) {
     }
 }
 */
-
+/*
 function interviewQuestion(job) { // job is available after interviewQuestion() has returned. It's used in the anonymous function below when it's called.
     return function(name) {
         if (job === 'designer') {
@@ -243,5 +243,148 @@ function interviewQuestion(job) { // job is available after interviewQuestion() 
 let interviewDesigner = interviewQuestion('designer');
 interviewDesigner('Doug');
 
-
+*/
 /* SOLUTION */
+
+/* CODING CHALLENGE */
+/*
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+
+        } else {
+            console.log('Wrong answer. Try again :)')
+        }
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                          ['Yes', 'No'],
+                          0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                          ['John', 'Micheal', 'Jonas'],
+                          2);
+
+    var q3 = new Question('What does best describe coding?',
+                          ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                          2);
+
+    var questions = [q1, q2, q3];
+
+    var n = Math.floor(Math.random() * questions.length);
+
+    questions[n].displayQuestion();
+
+    var answer = parseInt(prompt('Please select the correct answer.'));
+
+    questions[n].checkAnswer(answer);
+})();
+*/
+
+
+
+/* EXPERT LEVEL */
+
+
+
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+
+    // questions[n].checkAnswer(parseInt(answer), keepScore);
+    /*
+        questions[n] - calls array with the previously selected question n
+        checkAnswer is added to the Question class prototype, stores a function and has been called with parameters number: n and callback name: keepScore
+        sc declared in the checkAnswer scope, 
+   */
+    Question.prototype.checkAnswer = function(ans, callback) { 
+        var sc;
+        
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            sc = callback(true);
+        } else {
+            console.log('Wrong answer. Try again :)');
+            sc = callback(false);
+        }
+        
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('------------------------------');
+    }
+    
+    
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                          ['Yes', 'No'],
+                          0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                          ['John', 'Micheal', 'Jonas'],
+                          2);
+
+    var q3 = new Question('What does best describe coding?',
+                          ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                          2);
+    
+    var questions = [q1, q2, q3];
+    
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+    var keepScore = score();
+    
+    
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        if(answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);  // Chosen question n calls checkAnswer method with chosen answer and callback name 'keepScore' which holds score() which returns 
+                                                                    // an anonymous function accepting a boolean and increments the score if true  
+            
+            nextQuestion(); // calls itself, game loop?
+        }
+    }
+    
+    nextQuestion();
+    
+})();
