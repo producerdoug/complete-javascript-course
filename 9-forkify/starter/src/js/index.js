835f488711f8f8d3cc7e866fe7d737393e5398a3
Doug Allen <producerdoug@users.noreply.github.com>
2020-05-06 23:17:43 -0700

    Continue Section 9. Setup dev environment

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
-//import num from './test';
+import num from './test';
 
-//console.log(`I imported ${num} from another module!`);
+const x = 23;
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
